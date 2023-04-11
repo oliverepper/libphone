c612d316b3a7f6f369ad70742de63135c2985883
Oliver Epper <oliver.epper@gmail.com>
2023-04-11 18:14:13 +0200

    cleaner call functions

--- src/phone/include/private/account_t.h
+++ src/phone/include/private/account_t.h
@@ -20,17 +20,17 @@ public:
     void onRegState(pj::OnRegStateParam &prm) override {
         auto info = getInfo();
         if (info.regIsActive) {
-            PJ_LOG(3,(__BASE_FILE__, "register code: %d", prm.code));
+            PJ_LOG(3, (__BASE_FILE__, "register code: %d", prm.code));
         } else {
-            PJ_LOG(3,(__BASE_FILE__, "unregister code: %d", prm.code));
+            PJ_LOG(3, (__BASE_FILE__, "unregister code: %d", prm.code));
         }
         if (on_registration_state.has_value()) on_registration_state.value()(info.regIsActive, prm.code);
     }
 
     void onIncomingCall(pj::OnIncomingCallParam &prm) override {
-        PJ_LOG(3,(__BASE_FILE__, "Incoming call: %d", prm.callId));
+        PJ_LOG(3, (__BASE_FILE__, "Incoming call: %d", prm.callId));
         m_calls.emplace_back(std::make_unique<call_t>(*this,
-                                                      [this](int id){ delete_call(id); },
+                                                      [this](int id) { delete_call(id); },
                                                       prm.callId));
 
         auto rx_data = static_cast<pjsip_rx_data *>(prm.rdata.pjRxData);
@@ -38,16 +38,17 @@ public:
             // as an escape hatch save the complete INVITE message as a string
             m_calls.back()->incoming_message.emplace(rx_data->msg_info.msg_buf);
 
-            auto call_info_key = pj_str((char *)"Call-Info");
-            auto call_info_header = static_cast<pjsip_generic_string_hdr *>(pjsip_msg_find_hdr_by_name(rx_data->msg_info.msg, &call_info_key,
-                                                                                                       nullptr));
+            auto call_info_key = pj_str((char *) "Call-Info");
+            auto call_info_header = static_cast<pjsip_generic_string_hdr *>(pjsip_msg_find_hdr_by_name(
+                    rx_data->msg_info.msg, &call_info_key,
+                    nullptr));
             while (call_info_header != nullptr) {
                 /* IDEA maybe I should save the SERVER, too?
                  *  Call-Info: <sip:SERVER>;answer-after=0
                  *  std::optional<std::pair> answer-after
                  */
                 do {
-                    auto answer_after_key = pj_str((char *)"answer-after");
+                    auto answer_after_key = pj_str((char *) "answer-after");
                     char *pos = pj_stristr(&call_info_header->hvalue, &answer_after_key);
                     if (pos == nullptr)
                         break;
@@ -56,26 +57,29 @@ public:
                         break;
                     try {
                         m_calls.back()->answer_after.emplace(std::stoi(++pos));
-                    } catch (const std::invalid_argument& e) {
+                    } catch (const std::invalid_argument &e) {
                         PJ_LOG(1, (__BASE_FILE__, "%s", e.what()));
-                    } catch (const std::out_of_range& e) {
+                    } catch (const std::out_of_range &e) {
                         PJ_LOG(1, (__BASE_FILE__, "%s", e.what()));
                     }
                 } while (false);
                 if (m_calls.back()->answer_after.has_value())
                     break;
-                call_info_header = static_cast<pjsip_generic_string_hdr *>(pjsip_msg_find_hdr_by_name(rx_data->msg_info.msg, &call_info_key, call_info_header->next));
+                call_info_header = static_cast<pjsip_generic_string_hdr *>(pjsip_msg_find_hdr_by_name(
+                        rx_data->msg_info.msg, &call_info_key, call_info_header->next));
             }
         }
 
         m_calls.back()->on_call_state_with_index = on_call_state_with_index;
         m_calls.back()->on_call_state_with_id = on_call_state_with_id;
-        if (on_incoming_call_with_index.has_value()) on_incoming_call_with_index.value()(static_cast<int>(*m_calls.back()));
-        if (on_incoming_call_with_id.has_value()) on_incoming_call_with_id.value()(static_cast<std::string>(*m_calls.back()));
+        if (on_incoming_call_with_index.has_value())
+            on_incoming_call_with_index.value()(static_cast<int>(*m_calls.back()));
+        if (on_incoming_call_with_id.has_value())
+            on_incoming_call_with_id.value()(static_cast<std::string>(*m_calls.back()));
     }
 
-    void make_call(const std::string& uri) {
-        m_calls.emplace_back(std::make_unique<call_t>(*this, [this](int id){ delete_call(id); } ));
+    void make_call(const std::string &uri) {
+        m_calls.emplace_back(std::make_unique<call_t>(*this, [this](int id) { delete_call(id); }));
         m_calls.back()->on_call_state_with_index = on_call_state_with_index;
         m_calls.back()->on_call_state_with_id = on_call_state_with_id;
         pj::CallOpParam prm{true};
@@ -83,66 +87,66 @@ public:
         m_calls.back()->makeCall(uri, prm);
     }
 
-    auto call_for(phone::CallID auto id) {
-        return std::find_if(std::begin(m_calls), std::end(m_calls), [&id](const auto& element) {
+    std::vector<std::unique_ptr<call_t>>::iterator call_iterator(phone::CallID auto id) {
+        return std::find_if(std::begin(m_calls), std::end(m_calls), [&id](const auto &element) {
             return static_cast<decltype(id)>(*element) == id;
         });
     }
 
-    void answer_call(phone::CallID auto id) {
-        typename std::vector<std::unique_ptr<call_t>>::iterator it = call_for(id); //NOLINT(modernize-use-auto)
+    call_t *find_call(phone::CallID auto id) {
+        auto it = call_iterator(id);
         if (it != std::end(m_calls)) {
-            pj::CallOpParam prm;
-            prm.statusCode = PJSIP_SC_OK;
-            (*it)->answer(prm);
+            return it->get();
         }
+        if constexpr (std::is_same_v<decltype(id), int>) {
+            throw std::invalid_argument{"no call for index: <" + std::to_string(id) + ">"};
+        } else {
+            throw std::invalid_argument{"no call for id: <" + id + ">"};
+        }
+    }
+
+    void answer_call(phone::CallID auto id) {
+        call_t *call = find_call(id);
+        pj::CallOpParam prm;
+        prm.statusCode = PJSIP_SC_OK;
+        call->answer(prm);
     }
 
     void start_ringing_call(phone::CallID auto id) {
-        typename std::vector<std::unique_ptr<call_t>>::iterator it = call_for(id); //NOLINT(modernize-use-auto)
-        if (it != std::end(m_calls)) {
-            pj::CallOpParam prm;
-            prm.statusCode = PJSIP_SC_RINGING;
-            (*it)->answer(prm);
-        }
+        call_t *call = find_call(id);
+        pj::CallOpParam prm;
+        prm.statusCode = PJSIP_SC_RINGING;
+        call->answer(prm);
     }
 
     void hangup_call(phone::CallID auto id) {
-        typename std::vector<std::unique_ptr<call_t>>::iterator it = call_for(id); //NOLINT(modernize-use-auto)
-        if (it != std::end(m_calls)) {
-            pj::CallOpParam prm;
-            prm.statusCode = PJSIP_SC_DECLINE;
-            (*it)->hangup(prm);
-        }
+        call_t *call = find_call(id);
+        pj::CallOpParam prm;
+        prm.statusCode = PJSIP_SC_DECLINE;
+        call->hangup(prm);
     }
 
     std::optional<std::string> call_incoming_message(phone::CallID auto id) {
-        typename std::vector<std::unique_ptr<call_t>>::iterator it = call_for(id); //NOLINT(modernize-use-auto)
-        if (it != std::end(m_calls)) {
-            return (*it)->incoming_message;
-        }
-        if constexpr (std::is_same_v<decltype(id), int>) {
-            throw std::invalid_argument{"no call for index: <" + std::to_string(id) + ">"};
-        } else if constexpr (std::is_same_v<decltype(id), std::string>) {
-            throw std::invalid_argument{"no call for id: <" + id + ">"};
-        }
+        call_t *call = find_call(id);
+        return call->incoming_message;
     }
 
     std::optional<int> call_answer_after(phone::CallID auto id) {
-        typename std::vector<std::unique_ptr<call_t>>::iterator it = call_for(id); //NOLINT(modernize-use-auto)
-        if (it != std::end(m_calls)) {
-            return (*it)->answer_after;
-        }
-        if constexpr (std::is_same_v<decltype(id), int>) {
-            throw std::invalid_argument{"no call for index: <" + std::to_string(id) + ">"};
-        } else if constexpr (std::is_same_v<decltype(id), std::string>) {
-            throw std::invalid_argument{"no call for id: <" + id + ">"};
-        }
+        call_t *call = find_call(id);
+        return call->answer_after;
     }
 
-    void delete_call(int call_index) {
-        PJ_LOG(3,(__BASE_FILE__, "Going to delete call: %d", call_index));
-        auto it = call_for(call_index);
+    std::string get_call_id(int call_index) {
+        return static_cast<std::string>(*find_call(call_index));
+    }
+
+    int get_call_index(const std::string &call_id) {
+        return static_cast<int>(*find_call(call_id));
+    }
+
+    void delete_call(int call_index) noexcept {
+        PJ_LOG(3, (__BASE_FILE__, "Going to delete call: %d", call_index));
+        auto it = call_iterator(call_index);
         if (it != std::end(m_calls)) { m_calls.erase(it); }
     }
 
@@ -150,24 +154,8 @@ public:
         m_calls.clear();
     }
 
-    std::string get_call_id(int call_index) {
-        typename std::vector<std::unique_ptr<call_t>>::iterator it = call_for(call_index); //NOLINT(modernize-use-auto)
-        if (it != std::end(m_calls)) {
-            return static_cast<std::string>(*it->get()); //NOLINT(readability-redundant-smartptr-get)
-        }
-        throw std::invalid_argument{"no call for index: <" + std::to_string(call_index) + ">"};
-    }
-
-    int get_call_index(const std::string& call_id) {
-        typename std::vector<std::unique_ptr<call_t>>::iterator it = call_for(call_id); //NOLINT(modernize-use-auto)
-        if (it != std::end(m_calls)) {
-            return static_cast<int>(*it->get()); //NOLINT(readability-redundant-smartptr-get)
-        }
-        throw std::invalid_argument{"no call for id: <" + call_id + ">"};
-    }
-
 private:
     std::vector<std::unique_ptr<call_t>> m_calls{};
-} ;
+};
 
 #endif //ACCOUNT_T_H
